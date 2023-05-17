9241cbe852539e3812c9fda7dfc0a82c49ecc5bc
LeshA5 <alesha.glush2016@gmail.com>
2023-05-17 11:20:54 +0300

    7lab

--- cmd/blog/main.go
+++ cmd/blog/main.go
@@ -1,25 +1,45 @@
 package main
 
 import (
+	"database/sql"
+	"fmt"
 	"log"
 	"net/http"
+
+	_ "github.com/go-sql-driver/mysql" 
+	"github.com/jmoiron/sqlx"
+	"github.com/gorilla/mux"
 )
 
 const (
-	port = ":3000"
+	port = ":2000"
+	dbDriverName = "mysql"
 )
 
 func main() {
-	mux := http.NewServeMux()
-	mux.HandleFunc("/home", index)
-	mux.HandleFunc("/post", post)
-
-	// Реализуем отдачу статики
-	mux.Handle("/static/", http.StripPrefix("/static/", http.FileServer(http.Dir("./static"))))
-
-	log.Println("Start server " + port)
-	err := http.ListenAndServe(port, mux)
+	db, err := openDB()
 	if err != nil {
 		log.Fatal(err)
 	}
+
+	dbx := sqlx.NewDb(db, dbDriverName)
+    
+	mux := mux.NewRouter()
+	mux.HandleFunc("/home", index(dbx))
+	
+	mux.HandleFunc("/post/{postID}", post(dbx))
+
+	// Реализуем отдачу статики
+	mux.PathPrefix("/static/").Handler(http.StripPrefix("/static/", http.FileServer(http.Dir("./static"))))
+
+	fmt.Println("Start server ")
+	err = http.ListenAndServe(port, mux)
+	if err != nil {
+		log.Fatal(err)
+	}
+}
+
+func openDB() (*sql.DB, error) {
+	// Здесь прописываем соединение к базе данных
+	return sql.Open(dbDriverName, "root:Zomempaer5@tcp(localhost:3306)/blog?charset=utf8mb4&collation=utf8mb4_unicode_ci&parseTime=true")
 }
